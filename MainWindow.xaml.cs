9464e709577fb19823f21c6ae7b79d90b89cff13
Ярослав <yarikoverchenko@gmail.com>
2020-06-08 22:30:08 +0300

    Some new functions added

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Drawing;
 using System.Drawing.Imaging;
 using System.IO;
@@ -9,6 +10,7 @@ using System.Windows.Forms;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
+using MessageBox = System.Windows.MessageBox;
 using MouseEventHandler = System.Windows.Input.MouseEventHandler;
 using Path = System.Windows.Shapes.Path;
 using Point = System.Windows.Point;
@@ -134,34 +136,36 @@ namespace HaarImageCropper
                     DirectoryInfo dir = new DirectoryInfo(folder.SelectedPath);
                     SaveSamplesDir.Text = dir.FullName;
                 }
+                try
+                {
+                    if (!Directory.Exists($@"{SaveSamplesDir.Text}\Good"))
+                        Directory.CreateDirectory($@"{SaveSamplesDir.Text}\Good");
+                    if (!Directory.Exists($@"{SaveSamplesDir.Text}\Bad"))
+                        Directory.CreateDirectory($@"{SaveSamplesDir.Text}\Bad");
+                    DirectoryInfo dir = new DirectoryInfo($@"{SaveSamplesDir.Text}\Good");
+                    GoodSamplesCount.Content = dir.GetFiles().Length;
+                    dir = new DirectoryInfo($@"{SaveSamplesDir.Text}\Bad");
+                    BadSamplesCount.Content = dir.GetFiles().Length;
+                }
+                catch (Exception ex)
+                {
+                   MessageBox.Show($@"Creating samples directories error: {ex.Message}", "Error");
+                    return;
+                }
             }
         }
 
         private void SaveSample_Click(object sender, RoutedEventArgs e)
         {
             if (ImageCanvas.Children.OfType<Path>().Count() == 0)
-                System.Windows.MessageBox.Show("First crop a sample from image!");
+                MessageBox.Show("First crop a sample from image!");
             else if (!IsGoodSample.IsChecked.HasValue || !IsBadSample.IsChecked.HasValue)
-                System.Windows.MessageBox.Show("Select the type of the sample!");
+                MessageBox.Show("Select the type of the sample!");
             else if (string.IsNullOrEmpty(SaveSamplesDir.Text))
-                System.Windows.MessageBox.Show("Select a saving directories for the sample!");
-            else if (!OneObject.IsChecked.HasValue || !TwoObjects.IsChecked.HasValue || !ThreeObjects.IsChecked.HasValue)
-                System.Windows.MessageBox.Show("Select the number of the objects in the sample!");
+                MessageBox.Show("Select a saving directory for the samples!");
             else
             {
                 SaveSamplesDir.Text = SaveSamplesDir.Text.Trim(' ');
-                try 
-                {
-                    if (!Directory.Exists($@"{SaveSamplesDir.Text}\Good"))
-                        Directory.CreateDirectory($@"{SaveSamplesDir.Text}\Good");
-                    if (!Directory.Exists($@"{SaveSamplesDir.Text}\Bad"))
-                        Directory.CreateDirectory($@"{SaveSamplesDir.Text}\Bad");
-                }
-                catch (Exception ex)
-                {
-                    System.Windows.MessageBox.Show($@"Creating samples directories error: {ex.Message}", "Error");
-                    return;
-                }
                 foreach (Path sample in ImageCanvas.Children.OfType<Path>())
                 {
                     Rectangle sampleRect = new Rectangle((int)sample.Data.Bounds.X, (int)sample.Data.Bounds.Y, (int)sample.Data.Bounds.Width, (int)sample.Data.Bounds.Height);
@@ -172,23 +176,21 @@ namespace HaarImageCropper
                     ms.Flush();
                     Bitmap bmpImage = new Bitmap(ms);
                     Bitmap bmpCrop = bmpImage.Clone(sampleRect, bmpImage.PixelFormat);
+                    if (IsInGrayscale.IsChecked.HasValue && IsInGrayscale.IsChecked.Value)
+                    {
+                        bmpCrop = Extensions.CopyAsGrayscale(bmpCrop);
+                    }
                     if (IsGoodSample.IsChecked.Value)
                     {
                         DirectoryInfo dir = new DirectoryInfo($@"{SaveSamplesDir.Text}\Good");
                         int cropNumber = dir.GetFiles().Length;
                         bmpCrop.Save($@"{SaveSamplesDir.Text}\Good\{cropNumber}.bmp", ImageFormat.Bmp);
+                        GoodSamplesCount.Content = cropNumber + 1;
                         if (IsMarkupCreating.IsChecked.HasValue && IsMarkupCreating.IsChecked.Value)
                         {
-                            int objectsNum;
-                            if (OneObject.IsChecked.Value)
-                                objectsNum = 1;
-                            else if (TwoObjects.IsChecked.Value)
-                                objectsNum = 2;
-                            else
-                                objectsNum = 3;
                             using (StreamWriter sw = new StreamWriter($@"{SaveSamplesDir.Text}\Good.dat", true))
                             {
-                                sw.WriteLine($@"Good\{cropNumber}.bmp {objectsNum} 0 0 {bmpCrop.Width} {bmpCrop.Height}");
+                                sw.WriteLine($@"Good\{cropNumber}.bmp 1 0 0 {bmpCrop.Width} {bmpCrop.Height}");
                             }
                         }
                     }
@@ -197,19 +199,139 @@ namespace HaarImageCropper
                         DirectoryInfo dir = new DirectoryInfo($@"{SaveSamplesDir.Text}\Bad");
                         int cropNumber = dir.GetFiles().Length;
                         bmpCrop.Save($@"{SaveSamplesDir.Text}\Bad\{cropNumber}.bmp", ImageFormat.Bmp);
+                        BadSamplesCount.Content = cropNumber + 1;
                         if (IsMarkupCreating.IsChecked.HasValue && IsMarkupCreating.IsChecked.Value)
                         {
                             using (StreamWriter sw = new StreamWriter($@"{SaveSamplesDir.Text}\Bad.dat", true))
                             {
-                                sw.WriteLine($@"Bad\{cropNumber}.bmp");
+                                sw.WriteLine($@"{SaveSamplesDir.Text}\Bad\{cropNumber}.bmp");
                             }
                         }
                     }
                     ms.Dispose();
                 }
                 ImageCanvas.Children.Clear();
-                System.Windows.MessageBox.Show("All samples saved!");
+                MessageBox.Show("All samples saved!");
             }
+        }
+
+        private void ConvertExistingGrayscale_Click(object sender, RoutedEventArgs e)
+        {
+            if (!IsGoodSample.IsChecked.HasValue || !IsBadSample.IsChecked.HasValue)
+                MessageBox.Show("Select the type of the sample!");
+            else if (string.IsNullOrEmpty(SaveSamplesDir.Text))
+                MessageBox.Show("Select a saving directory for the samples!");
+            else
+            {
+                Directory.CreateDirectory($@"{SaveSamplesDir.Text}\Temp");
+                if (IsGoodSample.IsChecked.Value)
+                {
+                    DirectoryInfo dir = new DirectoryInfo($@"{SaveSamplesDir.Text}\Good");
+                    List<FileInfo> samples = new List<FileInfo>(dir.GetFiles());
+                    for (int i=0; i<samples.Count; i++)
+                    {
+                        Bitmap source = new Bitmap(samples[i].FullName);
+                        Bitmap result = Extensions.CopyAsGrayscale(source);
+                        source.Dispose();
+                        result.Save($@"{SaveSamplesDir.Text}\Temp\{samples[i].Name}", ImageFormat.Bmp);
+                        result.Dispose();
+                    }
+                    samples.Clear();
+                    dir.Delete(true);
+                    Directory.Move($@"{SaveSamplesDir.Text}\Temp", $@"{SaveSamplesDir.Text}\Good");
+                }
+                else if (IsBadSample.IsChecked.Value)
+                {
+                    DirectoryInfo dir = new DirectoryInfo($@"{SaveSamplesDir.Text}\Bad");
+                    List<FileInfo> samples = new List<FileInfo>(dir.GetFiles());
+                    for (int i = 0; i < samples.Count; i++)
+                    {
+                        Bitmap source = new Bitmap(samples[i].FullName);
+                        Bitmap result = Extensions.CopyAsGrayscale(source);
+                        source.Dispose();
+                        result.Save($@"{SaveSamplesDir.Text}\Temp\{samples[i].Name}", ImageFormat.Bmp);
+                        result.Dispose();
+                    }
+                    samples.Clear();
+                    dir.Delete(true);
+                    Directory.Move($@"{SaveSamplesDir.Text}\Temp", $@"{SaveSamplesDir.Text}\Bad");
+                }
+                MessageBox.Show("All samples converted!");
+            }
+        }
+
+        private void RenameSamples_Click(object sender, RoutedEventArgs e)
+        {
+            if (!IsGoodSample.IsChecked.HasValue || !IsBadSample.IsChecked.HasValue)
+                MessageBox.Show("Select the type of the sample!");
+            else if (string.IsNullOrEmpty(SaveSamplesDir.Text))
+                MessageBox.Show("Select a saving directory for the samples!");
+            else
+            {
+                Directory.CreateDirectory($@"{SaveSamplesDir.Text}\Temp");
+                if (IsGoodSample.IsChecked.Value)
+                {
+                    DirectoryInfo dir = new DirectoryInfo($@"{SaveSamplesDir.Text}\Good");
+                    List<FileInfo> samples = new List<FileInfo>(dir.GetFiles());
+                    for (int i = 0; i < samples.Count; i++)
+                    {
+                        samples[i].CopyTo($@"{SaveSamplesDir.Text}\Temp\{i}.bmp");
+                    }
+                    samples.Clear();
+                    dir.Delete(true);
+                    Directory.Move($@"{SaveSamplesDir.Text}\Temp", $@"{SaveSamplesDir.Text}\Good");
+                }
+                else if (IsBadSample.IsChecked.Value)
+                {
+                    DirectoryInfo dir = new DirectoryInfo($@"{SaveSamplesDir.Text}\Bad");
+                    List<FileInfo> samples = new List<FileInfo>(dir.GetFiles());
+                    for (int i = 0; i < samples.Count; i++)
+                    {
+                        samples[i].CopyTo($@"{SaveSamplesDir.Text}\Temp\{i}.bmp");
+                    }
+                    samples.Clear();
+                    dir.Delete(true);
+                    Directory.Move($@"{SaveSamplesDir.Text}\Temp", $@"{SaveSamplesDir.Text}\Bad");
+                }
+                MessageBox.Show("All samples renamed by the order!");
+            }
+        }
+
+        private void RewriteMarkup_Click(object sender, RoutedEventArgs e)
+        {
+            if (IsGoodSample.IsChecked.Value)
+            {
+                DirectoryInfo dir = new DirectoryInfo($@"{SaveSamplesDir.Text}\Good");
+                List<FileInfo> samples = new List<FileInfo>(dir.GetFiles());
+                if (File.Exists($@"{SaveSamplesDir.Text}\Good.dat"))
+                    File.Delete($@"{SaveSamplesDir.Text}\Good.dat");
+                for (int i = 0; i < samples.Count; i++)
+                {
+                    Bitmap image = new Bitmap(samples[i].FullName);
+                    using (StreamWriter sw = new StreamWriter($@"{SaveSamplesDir.Text}\Good.dat", true))
+                    {
+                        sw.WriteLine($@"Good\{samples[i].Name} 1 0 0 {image.Width} {image.Height}");
+                    }
+                    image.Dispose();
+                }
+                samples.Clear();
+            }
+            else if (IsBadSample.IsChecked.Value)
+            {
+                DirectoryInfo dir = new DirectoryInfo($@"{SaveSamplesDir.Text}\Bad");
+                List<FileInfo> samples = new List<FileInfo>(dir.GetFiles());
+                if (File.Exists($@"{SaveSamplesDir.Text}\Bad.dat"))
+                    File.Delete($@"{SaveSamplesDir.Text}\Bad.dat");
+                for (int i = 0; i < samples.Count; i++)
+                {
+                    using (StreamWriter sw = new StreamWriter($@"{SaveSamplesDir.Text}\Bad.dat", true))
+                    {
+                        sw.WriteLine($@"{SaveSamplesDir.Text}\Bad\{samples[i].Name}");
+                    }
+                }
+                samples.Clear();
+            }
+            MessageBox.Show("Markup file was rewritten!");
         }
     }
 }
